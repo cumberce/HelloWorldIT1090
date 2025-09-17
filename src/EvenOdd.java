6cd75af28ba4294aa68d0f5d7316f7aca19c856e
cumberce <cumberlandc03@gmail.com>
2025-09-17 12:07:30 -0400

    Added EvenOdd.java

---
+++ src/EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String [] args){
+        int number = 10;
+        boolean isEven = number % 2==0;
+        //number%2==0 will be true
+        boolean isOdd = number % 2!=0;
+
+    }
+}
